e45a703f85cef7f32fa77f8f47c4d1615f0d6b1a
Jenna Ransom <jenna.ransom@gmail.com>
2019-06-23 20:18:03 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -44,6 +44,6 @@ end
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(characters.series_id)
    FROM characters
-   INNER JOIN characters_books ON characters_books.character_id = characters.id
+   INNER JOIN character_books ON characters_books.character_id = characters.id
    ;"
 end
